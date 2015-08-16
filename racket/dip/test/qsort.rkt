5ebd354d374411dc6ca56538982938663de7921b
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2015-08-16 08:54:42 -0700

    fixed qsort.

--- racket/dip/test/qsort.rkt
+++ racket/dip/test/qsort.rkt
@@ -18,24 +18,18 @@
 (define (qfilter fcond pivot alon)
 	(cond
 		[(empty? alon) empty]
-		[else
-			(define fel (first alon))
-			(define rel (rest alon))
-			(cond
-				[(fcond fel pivot) (cons fel (aboveeqp pivot rel))]
-				[else (aboveeqp pivot rel)]
-			)
-		]
+		[(fcond pivot (first alon)) (cons (first alon) (qfilter fcond pivot (rest alon)))]
+		[else (qfilter fcond pivot (rest alon))]
 	)
 )
 
 ;; Gets all the elements that are above or equal to pivot.
 (define (aboveeqp pivot alon) 
-	(qfilter >= pivot alon))
+	(qfilter <= pivot alon))
 
 ;; Gets all the elements that are bellow to pivot.
 (define (belowp pivot alon) 
-	(qfilter < pivot alon))
+	(qfilter > pivot alon))
 
 (define (merge lower higher) 
 	(cond
@@ -46,6 +40,5 @@
 	)
 )
 
-(define mylon (list 12 4 5 535 66 3 3 2 88 33 99 36 985))
-
+(define mylon (list 2 1 3 5 3 33 67 81 5 33 77 45 99 47 28 75 39 78))
 (display (qsort mylon))
