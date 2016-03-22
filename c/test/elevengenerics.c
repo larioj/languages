fe8669a09c7a8b0abf736d56fc8186c782923260
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-03-22 00:53:39 -0700

    more fmt

--- c/test/elevengenerics.c
+++ c/test/elevengenerics.c
@@ -5,20 +5,24 @@
 		int: int_print, \
 		double: double_print)(x)
 
-void int_print(int val) {
-	printf("This is an int: %d\n", val);
+void int_print(int val)
+{
+    printf("This is an int: %d\n", val);
 }
 
-void double_print(double val) {
-	printf("This is a double: %f\n", val);
+void double_print(double val)
+{
+    printf("This is a double: %f\n", val);
 }
 
-void str_print(char *val) {
-	printf("This is a string: %s\n", val);
+void str_print(char *val)
+{
+    printf("This is a string: %s\n", val);
 }
 
-int main() {
-	gen_print((char *) "hello");
-	gen_print((int) 42);
-	return 0;
+int main()
+{
+    gen_print((char *) "hello");
+    gen_print((int) 42);
+    return 0;
 }
